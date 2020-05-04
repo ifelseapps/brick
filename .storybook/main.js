9816e953ba76e324ff146fc95d12599582eb5aa6
Maxim Pavlov <ifelseapps@gmail.com>
2020-05-04 17:40:19 +0300

    Jest installation

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,9 +9,6 @@ module.exports = {
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
       loader: require.resolve('babel-loader'),
-      options: {
-        presets: [['react-app', { flow: false, typescript: true }]],
-      },
     });
     config.module.rules.push({
       test: /\.scss$/,
